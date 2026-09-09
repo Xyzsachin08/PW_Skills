9985f57ebca9a6e1f259909ce892c0b7bed155c3
Sachin Borude <sachinborude0610@gmail.com>
2026-09-09 15:50:14 +0530

    list function and methods

---
+++ Practise Section/list.py
@@ -0,0 +1,14 @@
+l1 = [45, 78, 4, 5]
+l2 = [45,85,75]
+#l1.append(23)
+#l1.insert(1,10)
+#l1.extend(l2)
+#l1.remove(78)
+#l1.pop(1)
+#l1[1] = 80
+#l1.sort()
+nan = sorted(l1)
+#l1.clear()
+print(nan)
+print(id(l1))
+print(id(nan))
